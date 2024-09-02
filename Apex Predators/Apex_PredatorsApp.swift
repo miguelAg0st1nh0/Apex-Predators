c118bc2f08c502e3234ef27e31ca9168e0bf5513
Miguel Agostinho <miguel1995jordao@hotmail.com>
2024-09-02 21:28:42 +0100

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by Miguel Agostinho on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
