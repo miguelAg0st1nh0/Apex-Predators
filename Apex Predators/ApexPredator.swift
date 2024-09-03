7ba8a3fca59813962c550a2ec8fc0722062b9896
Miguel Agostinho <miguel1995jordao@hotmail.com>
2024-09-04 00:18:43 +0100

    Added some properties to Apex Predator class to customise predator type color within the content view. Added Navigation Stack and a Navigation Link as the basis of the main page functionality. Added a search bar to filter dinossaurs by name. Created a simple custom function to change dinossaur shadow image based on current color scheme.

--- Apex Predators/ApexPredator.swift
+++ Apex Predators/ApexPredator.swift
@@ -6,21 +6,26 @@
 //
 
 import Foundation
+import SwiftUI
 
 /// A struct that conforms to the JSON data, following a protocol of `Decodable`
 /// > Alert: variables need to match the name and format of those in the JSON file.
 /// Camel and Snake Cases are handled in a different swift file. If the JSON file has a variable: "movie_scenes"
 /// in snake case, this can be easily converted to camel case by using  ``.convertFromSnakeCase`` to accept "movieScenes".
-struct ApexPredator: Decodable {
+struct ApexPredator: Decodable, Identifiable {
     
     let id: Int
     let name: String
-    let type: String
+    let type: predatorType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [movieScene]
     let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
     
     ///A struct within ``ApexPredator`` to populate the array: ``movieScenes`` to conform to JSON file structure.
     ///This struct also needs to conform to the ``Decodable`` protocol.
@@ -29,6 +34,25 @@ struct ApexPredator: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+        
+    }
+    
+    
+    enum predatorType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var background: Color {
+            switch self {
+            case .land:
+                .green
+            case .air:
+                .mint
+            case .sea:
+                .blue
+            }
+        }
         
     }
     
--- Apex Predators/ContentView.swift
+++ Apex Predators/ContentView.swift
@@ -8,16 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @Environment(\.colorScheme) var darkMode
+    @State private var schemeColor: Color = .white
+    
+    @State private var searchText = ""
+    let predators = Predators()
+    
+    var filteredPredators: [ApexPredator] {
+        
+        if searchText.isEmpty {
+            return predators.apexPredators
+        } else {
+            return predators.apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        
+        
+        NavigationStack {
+            List(filteredPredators) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                        
+                    
+                } label: {
+                    
+                    HStack {
+                        //Dinossaur Image
+                        Image(predator.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .cornerRadius(10)
+                            .shadow(color: schemeColor, radius: 3)
+                        VStack(alignment: .leading) {
+                            //Dinossaur Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                                .fontDesign(.rounded)
+                                .font(.system(size: 20))
+                            //Dinossaur Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.vertical, 5)
+                                .padding(.horizontal, 13)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    } .onAppear(){
+                        getColorScheme()
+                    }
+                }
+                
+            }
+            .navigationTitle("Apex Predators  🦅")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.interactiveSpring, value: searchText)
+        }
+        
+    }
+    
+    
+    ///Custom function that automatically changes the dinossaur image shadow property to the inverse of the current color scheme.
+    ///
+    func getColorScheme() {
+        if darkMode == .dark{
+            schemeColor = .white
+        } else {
+            schemeColor = .black
+        }
     }
 }
+
+
 
 #Preview {
     ContentView()
